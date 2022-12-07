2b98de69d385e93bfaa1fb57bb7d14ed0cfeacad
Yang yuna <a33256448@gmail.com>
2022-12-08 02:56:18 +0900

    5

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,17 @@
 import Link from 'next/link'
 import styles from '../styles/Navbar.module.css'
+import React from 'react'
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
       <div className={styles.links}>
         <Link href="/">
-          양유나
+          <a>양유나</a>
         </Link>
-        <Link href="/about">
-          자기소개
-        </Link>
-        <Link href="/profile">
-          취미
-        </Link>
-        <Link href="/contact">
-          좋아하는 것들
-        </Link>
+        <Link href="/about"><<a>자기소개</a></Link>
+        <Link href="/profile">취미</Link>
+        <Link href="/contact">좋아하는 것들</Link>
       </div>
     </div>
-  );
+  )
 }
