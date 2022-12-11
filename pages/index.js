489f5067eb0352ad236d9dd72c550ff716c261be
Yang yuna <a33256448@gmail.com>
2022-12-11 23:07:35 +0900

    1211

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head'
-import Link from 'next/link'
+
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title> yuna's blog </title>
+        <title> myblog </title>
       </Head>
       <main className={styles.main}>
         <h1 className={styles.homeTitle}>안녕하세요</h1>
